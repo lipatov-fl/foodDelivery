295510a50c029d2fa7de972ca3294bbe1724972c
lipatov-fl <lipatov-fl@yandex.ru>
2024-01-04 16:48:53 +0400

    Переход на авторизацию

--- app/src/main/java/com/lipatovfl/fooddelivery/StartActivity.kt
+++ app/src/main/java/com/lipatovfl/fooddelivery/StartActivity.kt
@@ -1,11 +1,23 @@
 package com.lipatovfl.fooddelivery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.AppCompatButton
+import com.lipatovfl.fooddelivery.databinding.ActivityMainBinding
+import com.lipatovfl.fooddelivery.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_start)
+    lateinit var bindingClass : ActivityStartBinding
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
+        bindingClass = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        bindingClass.btnGoUserLogin.setOnClickListener {
+            val intent = Intent(this@StartActivity, LoginUserActivity :: class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
