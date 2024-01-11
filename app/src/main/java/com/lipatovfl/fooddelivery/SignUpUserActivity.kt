5c5284063ca09b0babe05df593a538b6361c058b
lipatov-fl <lipatov-fl@yandex.ru>
2024-01-11 20:32:51 +0400

    Внес правки в проект

--- app/src/main/java/com/lipatovfl/fooddelivery/SignUpUserActivity.kt
+++ app/src/main/java/com/lipatovfl/fooddelivery/SignUpUserActivity.kt
@@ -1,5 +1,11 @@
 package com.lipatovfl.fooddelivery
 
-class SignUpUserActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
+class SignUpUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_user)
+    }
 }
