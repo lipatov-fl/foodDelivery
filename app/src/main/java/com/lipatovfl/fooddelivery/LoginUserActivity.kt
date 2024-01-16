a5248b87d23ac8891720a7913bceed77c7aca2a9
lipatov-fl <lipatov-fl@yandex.ru>
2024-01-16 23:32:04 +0400

    Внес правки в проект

--- app/src/main/java/com/lipatovfl/fooddelivery/LoginUserActivity.kt
+++ app/src/main/java/com/lipatovfl/fooddelivery/LoginUserActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.lipatovfl.fooddelivery.databinding.ActivityLoginUserBinding
-import com.lipatovfl.fooddelivery.databinding.ActivityStartBinding
 
 class LoginUserActivity : AppCompatActivity() {
 
