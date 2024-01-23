af79cc2ba540b5b480dec1b3bb2789696e94d25e
lipatov-fl <lipatov-fl@yandex.ru>
2024-01-23 23:17:30 +0400

    Подготовка фрагментов к работе

--- app/src/main/java/com/lipatovfl/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/lipatovfl/fooddelivery/MainActivity.kt
@@ -2,10 +2,18 @@ package com.lipatovfl.fooddelivery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.navigation.findNavController
+import com.lipatovfl.fooddelivery.databinding.ActivityChooseLocationBinding
+import com.lipatovfl.fooddelivery.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    private lateinit var mainBinding: ActivityMainBinding
+    override fun onCreate(s: Bundle?) {
+        super.onCreate(s)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        var NavController = findNavController(R.id.fragmentContainerView)
+        var bottomNav = mainBinding.bNav
     }
 }
