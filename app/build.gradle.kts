46232ef55641e2b1194466046b90620ec298be96
lipatov-fl <lipatov-fl@yandex.ru>
2024-01-29 01:08:15 +0400

    Сверстал позицию recyclerView

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
 dependencies {
     implementation ("com.github.denzcoskun:ImageSlideshow:0.1.2")
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
