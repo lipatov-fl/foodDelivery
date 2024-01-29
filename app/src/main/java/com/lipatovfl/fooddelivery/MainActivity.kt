1b84b5b1181472c69d2c5ad199ef5542a9bb8fa8
lipatov-fl <lipatov-fl@yandex.ru>
2024-01-30 00:55:23 +0400

    Внес правки в cardView

--- app/src/main/java/com/lipatovfl/fooddelivery/MainActivity.kt
+++ app/src/main/java/com/lipatovfl/fooddelivery/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lipatovfl.fooddelivery
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -14,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(mainBinding.root)
         var NavController = findNavController(R.id.fragmentContainerView)
         val bottomNav = findViewById<BottomNavigationView>(R.id.bNav)
