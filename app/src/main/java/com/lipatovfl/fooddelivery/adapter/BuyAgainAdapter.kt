9e96a3a77004bc5226c70f430db8a88875fd33a2
Alexandr Lipatov <lipatov-fl@yandex.ru>
2024-02-17 18:23:14 +0300

    Подключил BuyAgainAdapter

---
+++ app/src/main/java/com/lipatovfl/fooddelivery/adapter/BuyAgainAdapter.kt
@@ -0,0 +1,41 @@
+package com.lipatovfl.fooddelivery.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lipatovfl.fooddelivery.R
+import com.lipatovfl.fooddelivery.databinding.BuyAgainItemBinding
+import com.lipatovfl.fooddelivery.databinding.CartItemBinding
+
+class BuyAgainAdapter(
+    private val buyAgainFoodName: ArrayList<String>,
+    private val buyAgainFoodPrice: ArrayList<String>,
+    private val buyAgainFoodImage: ArrayList<Int>
+) : RecyclerView.Adapter<BuyAgainAdapter.BuyAgainViewHolder>() {
+
+    class BuyAgainViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        private val binding = BuyAgainItemBinding.bind(item)
+        fun bind(foodName: String, foodPrice: String, foodImage: Int) = with(binding) {
+            binding.buyAgainFoodName.text = foodName
+            binding.buyAgainFoodPrice.text = foodPrice
+            binding.buyAgainFoodImage.setImageResource(foodImage)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BuyAgainViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.buy_again_item, parent, false)
+        return BuyAgainViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = buyAgainFoodName.size
+
+    override fun onBindViewHolder(holder: BuyAgainViewHolder, position: Int) {
+        holder.bind(
+            buyAgainFoodName[position],
+            buyAgainFoodPrice[position],
+            buyAgainFoodImage[position]
+        )
+    }
+}
