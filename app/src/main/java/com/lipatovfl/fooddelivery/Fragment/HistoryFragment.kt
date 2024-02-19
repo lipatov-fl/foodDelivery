a20f9423b4e27983d94cde420f5bd7a941b85189
Alexandr Lipatov <lipatov-fl@yandex.ru>
2024-02-19 19:28:15 +0300

    Закончил реализацию фрагмента History

--- app/src/main/java/com/lipatovfl/fooddelivery/Fragment/HistoryFragment.kt
+++ app/src/main/java/com/lipatovfl/fooddelivery/Fragment/HistoryFragment.kt
@@ -5,56 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.lipatovfl.fooddelivery.MenuBottomSheetFragment
 import com.lipatovfl.fooddelivery.R
+import com.lipatovfl.fooddelivery.adapter.BuyAgainAdapter
+import com.lipatovfl.fooddelivery.databinding.FragmentHistoryBinding
+import com.lipatovfl.fooddelivery.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HistoryFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HistoryFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var historyBinding: FragmentHistoryBinding
+    private lateinit var buyAgainAdapter: BuyAgainAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history, container, false)
+        historyBinding = FragmentHistoryBinding.inflate(inflater, container, false)
+        setupRecyclerView()
+        return historyBinding.root
+    }
+
+    private fun setupRecyclerView() {
+        val buyAgainFoodName =
+            arrayListOf("Herbal Pancake", "Fruit Salad", "Green Noddle", "Herbal Pancake", "Fruit Salad")
+        val buyAgainFoodPrice = arrayListOf("$5", "$7", "$4", "$9", "$5")
+        val buyAgainFoodImage = arrayListOf(
+            R.drawable.photo_menu1,
+            R.drawable.photo_menu2,
+            R.drawable.photo_menu3,
+            R.drawable.photo_menu1,
+            R.drawable.photo_menu2
+        )
+        buyAgainAdapter = BuyAgainAdapter(buyAgainFoodName, buyAgainFoodPrice, buyAgainFoodImage)
+        historyBinding.buyAgainRecyclerView.adapter = buyAgainAdapter
+        historyBinding.buyAgainRecyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HistoryFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HistoryFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
